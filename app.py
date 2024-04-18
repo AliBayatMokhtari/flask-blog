8cf95d7351dcd5bd66b42fddec8583bd36806b82
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2024-04-18 23:32:22 +0330

    chore: added init database method

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from flask import Flask
-from middlewares.auth_middleware import token_required
 from auth.auth import auth_blueprint
 from db.db import DatabaseConnection
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
-
   from config import SECRET_KEY, URL_PREFIX
 
   app.config["SECRET_KEY"] = SECRET_KEY
-
   app.register_blueprint(auth_blueprint, url_prefix=URL_PREFIX)
 
   db_connection = DatabaseConnection()
-
   db_connection.open_connection()
+
+  db_connection.init_database()
 
   app.run(host='localhost', port=8080, debug=True)
--- db/db.py
+++ db/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from helpers.SingletonMeta import SingletonMeta
 
 class DatabaseConnection(metaclass=SingletonMeta):
@@ -12,11 +13,29 @@ class DatabaseConnection(metaclass=SingletonMeta):
 
   def close_connection(self):
     if not self.connection:
-      raise "No Connection found..."
+      raise "No open connection found..."
     self.connection.close()
     self.connection = None
+    print("Connection closed successfully.")
 
   def get_connection(self):
     return self.connection
+  
+  def get_script(script_name: str):
+    script_dir = os.path.dirname(__file__)
+    rel_path = "queries/" + script_name
+    abs_path = os.path.join(script_dir, rel_path)
+    return abs_path
+  
+  def execute_query(self, script_name: str, msg: str):
+    script_path = DatabaseConnection.get_script(script_name)
+    with open(script_path) as q:
+      self.connection.executescript(q.read())
+      print(msg)
 
-connection_1 = DatabaseConnection()
+  def init_database(self):
+    if not self.connection:
+      self.open_connection()
+    script_name = "InitDB.sql"
+    msg = "Database initiated successfully."
+    self.execute_query(script_name, msg)
